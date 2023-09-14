54491600407ce5ed66b64a589cb53c74505a46b6
T08Y02 <pon9q9@gmail.com>
2023-09-14 14:49:19 +0900

    録音の保存と再生

--- src/page/Kagaribi.js
+++ src/page/Kagaribi.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef }  from 'react';
 import { useReactMediaRecorder } from "react-media-recorder";
 import firebase from "firebase/app";
 import { storage } from "../firebase";
-import { getStorage, ref, uploadBytes } from "firebase/storage";
+import { getStorage, ref, uploadBytes, listAll, getDownloadURL} from "firebase/storage";
 
 import axios from "axios";
 
@@ -34,7 +34,7 @@ function Kagaribi() {
     const getBlobFromBlobURL = async () => {
         console.log(mediaBlobUrl)
         try {
-            var response = await axios.get(mediaBlobUrl, {
+            const response = await axios.get(mediaBlobUrl, {
             responseType: 'blob' // レスポンスのデータ型をblobに指定
             });
     
@@ -80,16 +80,21 @@ function Kagaribi() {
 
     //以下は保存処理
     const saveWavFile = async() => {
-        var myblob =  await getBlobFromBlobURL();
-        var mywav = await getWavfromBlob(myblob);
+        const myblob =  await getBlobFromBlobURL();
+        const mywav = await getWavfromBlob(myblob);
         const myaudio = await new Audio(URL.createObjectURL(mywav));
+        myaudio.name = "happy" + Math.random().toString(32).substring(2);
+
         const storageRef = ref(storage, `audios/${myaudio.name}`);
-        var myblob =  await getBlobFromBlobURL();
-        var mywav = await getWavfromBlob(myblob);
         console.log(mywav);
         const audio = await new Audio(URL.createObjectURL(mywav));
         await audio.play();
-        await uploadBytes(storageRef, myaudio)
+
+        const metadata = {
+            contentType: 'audio/wav',
+        };
+
+        await uploadBytes(storageRef, mywav)
             .then((snapshot) => {
                 console.log("アップロードに成功しました");
             })
@@ -99,38 +104,40 @@ function Kagaribi() {
         
     }
 
-    
-    // 再描画の影響を受けない不変なオブジェクト
-    const audioContext = useRef(null);
-    // 内部状態
-    const [audioBuffer, setAudioBuffer] = useState(null); // 追加
+    async function getAllWavPath(){
+        const listRef = ref(storage, `audios`);
+        const path_list = [];
+        await listAll(listRef)
+        .then((res) => {
+            res.prefixes.forEach((folderRef) => {
+            // All the prefixes under listRef.
+            // You may call listAll() recursively on them.
+            });
+            res.items.forEach(async (itemRef) => {
+                //console.log(itemRef);
+                await path_list.push(itemRef._location.path_);
 
-    // 初期化
-    useEffect(()=>{
-        audioContext.current = new AudioContext();
-    },[])
-    // イベントコールバック
-    const handleChangeFile = async(event) => {
-        const _file = event.target.files[0];
-        const _audioBuffer = await audioContext.current.decodeAudioData( // 追加
-        await _file.arrayBuffer()
-        );
-        setAudioBuffer(_audioBuffer); // 追加
-    };
+            });
+        }).catch((error) => {
+            // Uh-oh, an error occurred!
+        });
+        return path_list;
+    }
 
-    const handleClickPlay = () => { 
-        // 自動再生ブロックにより停止されたオーディオを再開させる
-        if (audioContext.current.state === "suspended") {
-          audioContext.current.resume();
-        }
-        // ソースノード生成 ＋ 音声を設定
-        const sourceNode = audioContext.current.createBufferSource();
-        //sourceNode.buffer = audioBuffer;
-        // 出力先に接続
-        sourceNode.connect(audioContext.current.destination);
-        // 再生発火
-        sourceNode.start();
-        };
+    async function selectSound(){
+        const firestorage = storage;
+        const path_list = await getAllWavPath();
+        const selected_sound_path = path_list[Math.floor( Math.random() * path_list.length)];
+        console.log(selected_sound_path);
+        getDownloadURL(ref(storage, selected_sound_path))
+        .then((url) => {
+            console.log(url);
+            const audio = new Audio(url);
+            audio.play();
+
+        });
+        
+    }
 
     return (
         <div>
@@ -142,6 +149,10 @@ function Kagaribi() {
 
             <div>
                 <button onClick={saveWavFile}>送信</button>
+            </div>
+
+            <div>
+                <button onClick={selectSound}>再生</button>
             </div>
 
             <div className="bg-red-400">
